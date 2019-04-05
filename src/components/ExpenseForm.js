44441f7433ba52c00a8bf08c8fbe41e9c462aa10
Daniel Niko <daniel.gultom@gmail.com>
2019-04-05 16:59:16 +0700

    add form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import SkyLight from 'react-skylight';
+import { SERVER_URL } from '../constants.js'
 
 class ExpenseForm extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { name: '', description: '', category: '', amount: '' };
+        this.state = {
+            name: '', description: '', amount: '',
+            categoryId: '', 
+            categories : []
+        };
+    }
+
+    componentDidMount() {
+        this.fetchCategories();
     }
 
     handleChange = (event) => {
@@ -14,13 +23,28 @@ class ExpenseForm extends React.Component {
         );
     }
 
+    fetchCategories = () => {
+        fetch(SERVER_URL + 'category')
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    categories: responseData,
+                    categoryId: responseData[0].categoryId
+                });
+            })
+            .catch(err => console.error(err));
+    }
+
     // Save expense and close modal form
     handleSubmit = (event) => {
         event.preventDefault();
+        var category = {
+            categoryId: this.state.categoryId
+        };
         var newExpense = {
-            name: this.state.name, 
+            name: this.state.name,
             description: this.state.description,
-            category: this.state.category, 
+            category: category,
             amount: this.state.amount
         };
         this.props.addExpense(newExpense);
@@ -39,12 +63,16 @@ class ExpenseForm extends React.Component {
                 <SkyLight hideOnOverlayClicked ref="addDialog">
                     <h3>New Expense</h3>
                     <form>
-                        <input type="text" placeholder="Name" name="name"
+                        <input type="text" placeholder="Name" name="name" value={this.state.name}
                             onChange={this.handleChange} /><br />
-                        <input type="text" placeholder="Description" name="description"
+                        <input type="text" placeholder="Description" name="description" value={this.state.description}
                             onChange={this.handleChange} /><br />
-                        <input type="text" placeholder="Category" name="category"
-                            onChange={this.handleChange} /><br />
+                        <select name="categoryId" value={this.state.categoryId}
+                            onChange={this.handleChange}>
+                            {this.state.categories.map(
+                                (category) => <option key={category.categoryId} value={category.categoryId}>{category.name}</option>
+                            )}
+                        </select><br />
                         <input type="text" placeholder="Amount" name="amount"
                             onChange={this.handleChange} /><br />
                         <button onClick={this.handleSubmit}>Save</button>
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,6 +3,8 @@ import { SERVER_URL } from '../constants.js'
 import ReactTable from "react-table";
 import 'react-table/react-table.css';
 import ExpenseForm from './ExpenseForm.js';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class ExpenseList extends Component {
 
@@ -17,7 +19,7 @@ class ExpenseList extends Component {
 
     // Add new expense
     addExpense(expense) {
-        fetch(SERVER_URL + 'expense',
+        fetch(SERVER_URL + 'expense/add',
             {
                 method: 'POST',
                 headers: {
@@ -29,8 +31,26 @@ class ExpenseList extends Component {
             .catch(err => console.error(err))
     }
 
+    // Delete expense
+    onDelClick = (link) => {
+        fetch(SERVER_URL + 'expense/' + link,
+            { method: 'DELETE' })
+            .then(res => {
+                toast.success("Expense deleted", {
+                    position: toast.POSITION.BOTTOM_LEFT
+                });
+                this.fetchExpenses();
+            })
+            .catch(err => {
+                toast.error("Error when deleting", {
+                    position: toast.POSITION.BOTTOM_LEFT
+                });
+                console.error(err)
+            })
+    }
+
     fetchExpenses = () => {
-        fetch(SERVER_URL + 'expenses')
+        fetch(SERVER_URL + 'expense')
             .then((response) => response.json())
             .then((responseData) => {
                 this.setState({
@@ -53,13 +73,21 @@ class ExpenseList extends Component {
         }, {
             Header: 'Amount',
             accessor: 'amount',
+        }, {
+            id: 'delbutton',
+            sortable: false,
+            filterable: false,
+            width: 100,
+            accessor: 'expenseId',
+            Cell: ({ value }) => (<button onClick={() => { this.onDelClick(value) }}>Delete</button>)
         }]
 
         return (
             <div className="App">
-                <ExpenseForm addExpense={this.addExpense} fetchExpenses={this.fetchExpenses}/>
+                <ExpenseForm addExpense={this.addExpense} fetchExpenses={this.fetchExpenses} />
                 <ReactTable data={this.state.expenses} columns={columns}
                     filterable={true} />
+                <ToastContainer autoClose={1500} />
             </div>
         );
     }
